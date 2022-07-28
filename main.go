9183f9a41264e0175a7d4b7911e7973bad7daf1a
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2022-07-28 10:34:43 -0400

    adding files functions and handlers

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+}
+
+// addValues adds two intergers and returns the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main is the main application function
+func main() {
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+
+	_ = http.ListenAndServe(portNumber, nil)
+}

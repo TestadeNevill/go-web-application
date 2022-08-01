87328eb0e1eb99c450befc830b5f967daaefbd12
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2022-08-01 09:53:57 -0400

    Enabling go modules and refractoring the code to use packages

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module myapp
+module github.com/testadenevill/go-web-application
 
 go 1.18
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,16 +1,18 @@
-package main
+package handlers
 
 import (
 	"net/http"
+
+	"github.com/testadenevill/go-web-application/pkg/render"
 )
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.html")
+	render.RenderTemplate(w, "home.page.html")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.html")
+	render.RenderTemplate(w, "about.page.html")
 
 }
